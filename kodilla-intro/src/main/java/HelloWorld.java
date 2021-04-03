e739b966a0e89d1db99c1b574083f439c7c44ba4
MartaKedzior <marta.kedzior@wp.pl>
2021-04-03 20:56:24 +0200

    Lesson 0.7

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Marta");
+        System.out.println("00");
+        System.out.println("KK");
+    }
+}
