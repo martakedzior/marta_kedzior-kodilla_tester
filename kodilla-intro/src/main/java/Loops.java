80d01c5b416507596bb10a637d5f22016ade1b4b
MartaKedzior <marta.kedzior@wp.pl>
2021-04-06 14:40:13 +0200

    For Loop

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -3,5 +3,18 @@ public class Loops {
         for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
+
+        int [] myTable = {1, 2, 566, 332, -3};
+        System.out.println(sumTableElements(myTable));
+
     }
+
+    public static int sumTableElements(int[] myTable) {
+        int sumOfTableElements = 0;
+        for (int i = 0; i < myTable.length; i++) {
+            sumOfTableElements = myTable[i] + sumOfTableElements;
+        }
+        return  sumOfTableElements;
+    }
+
 }
