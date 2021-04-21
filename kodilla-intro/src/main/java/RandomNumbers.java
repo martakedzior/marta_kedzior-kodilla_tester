24d564acdcefcf4d080de55a5a314b363b22e7ce
MartaKedzior <marta.kedzior@wp.pl>
2021-04-21 14:25:44 +0200

    Exercise 2.6 - after code refactoring with mentor

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -2,33 +2,29 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class RandomNumbers {
-    private int [] randomNumbers;
-    private int size;
 
-    public RandomNumbers() {
-        this.randomNumbers = new int[1000];
-        this.size = 0;
-    }
-
-    public void getSumOfRandomNumbers() {
+    public int [] getSumOfRandomNumbers() {
+        int [] randomNumbers = new int[1000];
         Random randomNumber = new Random();
+        int size = 0;
         int sum = 0;
 
         while (sum < 5000) {
             int temp = randomNumber.nextInt(30);
-            this.randomNumbers[this.size] = temp;
+            randomNumbers[size] = temp;
             sum = sum + temp;
-            this.size++;
+            size++;
         }
+        return randomNumbers;
     }
 
-    public void minNumber(){
+    public void minNumber(int [] randomNumbers){
         int minNumber;
         minNumber = Arrays.stream(randomNumbers).min().getAsInt();
         System.out.println("Minimalna wylosowana wartość to: "+ minNumber);
     }
 
-    public void maxNumber(){
+    public void maxNumber(int [] randomNumbers){
         int minNumber;
         minNumber = Arrays.stream(randomNumbers).max().getAsInt();
         System.out.println("Maksymalna wylosowana wartość to: "+ minNumber);
@@ -36,9 +32,9 @@ public class RandomNumbers {
 
     public static void main(String[] args) {
         RandomNumbers randomNumbers = new RandomNumbers();
-        randomNumbers.getSumOfRandomNumbers();
-        randomNumbers.minNumber();
-        randomNumbers.maxNumber();
+        int [] number = randomNumbers.getSumOfRandomNumbers();
+        randomNumbers.minNumber(number);
+        randomNumbers.maxNumber(number);
 
     }
 }
