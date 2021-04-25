93ff64147ec2170fb1ecfe90ccd48641f16af7d0
MartaKedzior <marta.kedzior@wp.pl>
2021-04-25 21:49:53 +0200

    2.9 Switch examples

---
+++ kodilla-intro/src/main/java/AdvCalcApplication.java
@@ -0,0 +1,8 @@
+public class AdvCalcApplication {
+
+    public static void main(String[] args) {
+        AdvCalculator calculator = new AdvCalculator();
+        double result = calculator.calculate();
+        System.out.println("Result: " + result);
+    }
+}
