d80ba0a81d02227ddc52e56ee89510773ffdd643
MartaKedzior <marta.kedzior@wp.pl>
2021-04-25 21:23:40 +0200

    2.7 Exercise

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,13 +2,20 @@ public class Book {
     private String author;
     private String title;
 
-    static Book of(String author, String title){
-        Book book = new Book();
+    public Book(String author, String title){
+        this.title = title;
+        this.author = author;
+    }
+
+    public static Book of(String author, String title){
+        Book book = new Book(author, title);
         return book;
     }
 
     public static void main(String[] args){
         Book book = Book.of("Isaac Asimov", "The Galaxy");
-
+        System.out.println(book.author + " --> " + book.title);
     }
 }
+
+
