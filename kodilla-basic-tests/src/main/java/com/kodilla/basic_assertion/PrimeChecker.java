40bf8b0c536cb9c73df2b217a20309fe69797841
MartaKedzior <marta.kedzior@wp.pl>
2021-05-03 20:08:27 +0200

    3.7. Test Driven Development

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/PrimeChecker.java
@@ -0,0 +1,20 @@
+package com.kodilla.basic_assertion;
+
+public class PrimeChecker {
+
+
+    public boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+}
