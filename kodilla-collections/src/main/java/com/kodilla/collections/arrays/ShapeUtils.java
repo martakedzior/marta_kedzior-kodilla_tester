1d5d12b2d950f09126d12543a114706e15d867c4
MartaKedzior <marta.kedzior@wp.pl>
2021-05-07 16:22:42 +0200

    4.2. Shape Utils update

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/ShapeUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/ShapeUtils.java
@@ -7,10 +7,11 @@ import com.kodilla.collections.interfaces.Triangle;
 
 public class ShapeUtils {
     public static void displayShapeInfo(Shape shape) {
-        System.out.println("-----------------------------");
+        System.out.println("---------------------------------");
         System.out.println("Shape kind: " + getShapeName(shape));
-        System.out.println("Shape area: " + shape.getArea());
+        System.out.println("Shape area : " + shape.getArea());
         System.out.println("Shape perimeter: " + shape.getPerimeter());
+
     }
 
     private static String getShapeName(Shape shape) {
