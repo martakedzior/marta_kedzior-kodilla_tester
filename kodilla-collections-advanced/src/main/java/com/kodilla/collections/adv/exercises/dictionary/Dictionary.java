98ca497e0b113db74bab404e2c50193e2a7f2839
Marta Kędzior-Krasoczko <marta.kedzior@wp.pl>
2021-05-20 15:40:49 +0200

    5.5 Unit tests with collections

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictionary/Dictionary.java
@@ -0,0 +1,33 @@
+package com.kodilla.collections.adv.exercises.dictionary;
+
+import java.util.*;
+
+public class Dictionary {
+
+    Map<String, List<EnglishWord>> dictionary = new HashMap<>();
+
+    public void addWord(String polishWord, EnglishWord englishWord) {
+        List<EnglishWord> englishWords = dictionary.getOrDefault(polishWord, new ArrayList<>());
+        englishWords.add(englishWord);
+        dictionary.put(polishWord, englishWords);
+
+    }
+
+    public List<EnglishWord> findEnglishWords(String polishWord) {
+        return dictionary.getOrDefault(polishWord, Collections.emptyList());
+
+    }
+
+    public List<EnglishWord> findEnglishWords(String polishWord, PartOfSpeech partOfSpeech) {
+        List<EnglishWord> result = new ArrayList<>();
+        for (EnglishWord englishWord : dictionary.getOrDefault(polishWord, Collections.emptyList())){
+            if (englishWord.getPartOfSpeech().equals(partOfSpeech))
+                result.add(englishWord);
+        }
+        return result;
+    }
+
+    public int size() {
+        return dictionary.size();
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictionary/EnglishWord.java
@@ -0,0 +1,34 @@
+package com.kodilla.collections.adv.exercises.dictionary;
+
+import java.util.Objects;
+
+public class EnglishWord {
+    private PartOfSpeech partOfSpeech;
+    private String word;
+
+    public EnglishWord(PartOfSpeech partOfSpeech, String word) {
+        this.partOfSpeech = partOfSpeech;
+        this.word = word;
+    }
+
+    public PartOfSpeech getPartOfSpeech() {
+        return partOfSpeech;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnglishWord that = (EnglishWord) o;
+        return partOfSpeech == that.partOfSpeech && Objects.equals(word, that.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(partOfSpeech, word);
+    }
+}
