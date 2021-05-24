c3630d9a6b011e9c181bfe47c1f98c0ecb664959
Marta Kędzior-Krasoczko <marta.kedzior@wp.pl>
2021-05-24 15:01:15 +0200

    6.4 Exceptions - examples

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/exception/FirstExample.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.exception;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        String username = "John";
+        String[] nameParts = username.split(" ");
+        System.out.println("Firstname: " + nameParts[0]);
+        System.out.println("Lastname: " + nameParts[1]);
+
+    }
+}
