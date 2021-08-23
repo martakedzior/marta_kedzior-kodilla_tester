0efc672b9d0cef6e44361bd3acec17a335a50605
Marta Kędzior-Krasoczko <marta.kedzior@wp.pl>
2021-08-23 21:58:52 +0200

    10.3 REST API creation - examples and homework

---
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<BookDto> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @PostMapping
+    public BookDto addBook(@RequestBody BookDto bookDto) {
+        bookService.addBook(bookDto);
+        return bookDto;
+    }
+
+    @DeleteMapping
+    public void removeBook(@RequestBody BookDto bookDto) {
+        bookService.removeBook(bookDto);
+    }
+
+}
